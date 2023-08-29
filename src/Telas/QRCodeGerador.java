e36aee4b0deb29fb5da5743957b97cc0257a1cc6
N4thz <106750350+N4thz@users.noreply.github.com>
2023-08-28 22:45:22 -0300

    Atualizando QR Code Signed-off-by:N4thz <106750350+N4thz@users.noreply.github.com>

--- src/Telas/QRCodeGerador.java
+++ src/Telas/QRCodeGerador.java
@@ -19,13 +19,14 @@ public class QRCodeGerador extends javax.swing.JDialog {
     /**
      * Creates new form QRCodeGerador
      */
-    String ConteudoQRCode;
+    String ConteudoQRCode, TituloQRCode;
     
-    public QRCodeGerador(JFrame QRCodeGerador, boolean par, String valor) {
+    public QRCodeGerador(JFrame QRCodeGerador, boolean par, String conteudo, String titulo) {
         super(QRCodeGerador, "Senha em QR Code", true);
-        ConteudoQRCode = valor;
+        ConteudoQRCode = conteudo;
+        TituloQRCode = titulo;
         initComponents();
-        GerarQRCode(ConteudoQRCode);
+        GerarQRCode(ConteudoQRCode, TituloQRCode);
     }
     
     @SuppressWarnings("unchecked")
@@ -35,9 +36,11 @@ public class QRCodeGerador extends javax.swing.JDialog {
         jPanel1 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
+        jButton1.setFont(new java.awt.Font("Dialog", 1, 11)); // NOI18N
         jButton1.setText("Sair");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -45,24 +48,33 @@ public class QRCodeGerador extends javax.swing.JDialog {
             }
         });
 
+        jLabel2.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText(" ");
+        jLabel2.setToolTipText("");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 398, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 343, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 330, Short.MAX_VALUE)
-                .addGap(18, 18, 18)
+                .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 301, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton1)
                 .addContainerGap())
         );
@@ -114,7 +126,7 @@ public class QRCodeGerador extends javax.swing.JDialog {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> {
-            QRCodeGerador dialog = new QRCodeGerador(new javax.swing.JFrame(), true, "");
+            QRCodeGerador dialog = new QRCodeGerador(new javax.swing.JFrame(), true, "", "");
             dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                 @Override
                 public void windowClosing(java.awt.event.WindowEvent e) {
@@ -128,6 +140,7 @@ public class QRCodeGerador extends javax.swing.JDialog {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
     
@@ -135,7 +148,7 @@ public class QRCodeGerador extends javax.swing.JDialog {
         return ConteudoQRCode;
     }
     
-    private void GerarQRCode(String conteudo){
+    private void GerarQRCode(String conteudo, String titulo){
         int sizeH = jLabel1.getHeight();
         int sizeW = jLabel1.getWidth();
         try {
@@ -143,6 +156,7 @@ public class QRCodeGerador extends javax.swing.JDialog {
             byte[] QRByte = out.toByteArray();
             ImageIcon QR = new ImageIcon(QRByte);
             jLabel1.setIcon(QR);
+            jLabel2.setText(titulo);
         }
         catch(Exception ex){      
         }
